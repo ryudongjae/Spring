fb4c7f450fcaf059c4c5c2627b3e146ef604f126
ryudongjae <rdj10149@gmail.com>
2022-01-09 23:03:34 +0900

    Initial commit

---
+++ src/main/java/com/ryu/QuerydslProject/QuerydslProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ryu.QuerydslProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryu/QuerydslProject/QuerydslProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryu.QuerydslProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
