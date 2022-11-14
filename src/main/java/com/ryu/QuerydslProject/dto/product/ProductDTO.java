cf0866d9f5e65be7419298b8ee836d94728c6333
ryudongjae <rdj10149@gmail.com>
2022-11-14 21:22:09 +0900

    product Service

--- src/main/java/com/ryu/QuerydslProject/dto/product/ProductDTO.java
+++ src/main/java/com/ryu/QuerydslProject/dto/product/ProductDTO.java
@@ -1,4 +1,23 @@
 package com.ryu.QuerydslProject.dto.product;
 
 public class ProductDTO {
+
+    public static class SaveRequest {
+        private String name;
+
+        private int price;
+
+        private String information;
+
+    }
+
+
+    public static class UpdateRequest {
+        private String name;
+
+        private int price;
+
+        private String information;
+
+    }
 }
